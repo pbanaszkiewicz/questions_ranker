a21c1fdb39eb5c94d33c24a7f5957faa0d35187b
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-10-06 11:25:14 +0200

    Switch from REDIS to database cache

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,14 +18,16 @@ DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # no
 # ------------------------------------------------------------------------------
 CACHES = {
     'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': env('REDIS_URL'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            # Mimicing memcache behavior.
-            # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-            'IGNORE_EXCEPTIONS': True,
-        }
+        # 'BACKEND': 'django_redis.cache.RedisCache',
+        # 'LOCATION': env('REDIS_URL'),
+        # 'OPTIONS': {
+        #     'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        #     # Mimicing memcache behavior.
+        #     # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+        #     'IGNORE_EXCEPTIONS': True,
+        # }
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'questions_ranker_cache',
     }
 }
 
