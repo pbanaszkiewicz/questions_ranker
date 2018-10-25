0fef18b62fcfa9be227a687fb53e8c4d8718a524
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-10-25 20:36:25 +0200

    bulk_add_questions: add option for bulk updating (via a switch)

--- questions.py
+++ questions.py
@@ -5,7 +5,7 @@ from questions_ranker.users.models import User
 
 
 @transaction.atomic
-def bulk_add_questions(filename):
+def bulk_add_questions(filename, update=False):
     admin = User.objects.get(username="admin")
     with open(filename, 'r') as f:
         reader = csv.DictReader(f, delimiter=',', quotechar='"')
@@ -13,11 +13,18 @@ def bulk_add_questions(filename):
         for row in reader:
             cat, _ = Category.objects.get_or_create(name=row['Category'],
                                                     defaults=dict(author=admin))
-            res = Question.objects.create(
-                id=int(row['ID']),
-                content=row['Question'],
-                category=cat,
-                author=admin,
-            )
+            if update:
+                res = Question.objects.filter(id=int(row['ID'])).update(
+                    category=cat,
+                    content=row['Question'],
+                    author=admin,
+                )
+            else:
+                res = Question.objects.create(
+                    id=int(row['ID']),
+                    content=row['Question'],
+                    category=cat,
+                    author=admin,
+                )
             results.append(res)
     return results
